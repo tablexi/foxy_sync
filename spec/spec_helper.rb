1505a3cd520d994a500fbb5deb7080d54695d1e3
Chris Stump <chris@mighty-nice.com>
2013-01-28 20:44:20 -0600

    move test XML into helper; add loading of support files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require 'rspec'
 require File.expand_path '../lib/foxy_sync.rb', File.dirname(__FILE__)
 
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[ File.expand_path './support/**/*.rb', File.dirname(__FILE__) ].each {|f| require f}
+
 RSpec.configure do |config|
  # Run specs in random order to surface order dependencies. If you find an
  # order dependency and want to debug it, you can fix the order by providing
