ccf560a87dbd86b441d4cbac20dc84c7ea40b1d0
Chris Stump <chris@mighty-nice.com>
2013-02-01 17:16:52 -0600

    SSO impl

--- lib/foxy_sync/sso.rb
+++ lib/foxy_sync/sso.rb
@@ -1,5 +1,46 @@
+require 'digest/sha1'
+
 module FoxySync
+  #
+  # Holds methods that help with FoxyCart's SSO service
+  # (http://wiki.foxycart.com/v/1.0/sso). This module is
+  # intended to be mixed into a controller for easy completion
+  # of the SSO handshake.
   module Sso
+    #
+    # Returns the redirect URL that FoxyCart
+    # expects to be requested after it hits your
+    # application's SSO URL.
+    # [_params_]
+    #   Something that responds to [] and holds the
+    #   'fcsid' and 'timestamp' parameters given by
+    #   the FoxyCart SSO request. Usually your controller's
+    #   +params+ object.
+    # [_user_]
+    #   The currently logged in user, if any
+    def sso_url(params, user = nil)
+      fc_session_id = params['fcsid']
+      cid = 0
+
+      if user
+        customer = FoxySync::Api::Customer.new user
+        cid = customer.customer_id
+        cid = customer.save.customer_id if cid.nil?
+      end
+
+      timestamp = params['timestamp'].to_i
+      later_timestamp = timestamp + 60 * 60
+      token = auth_token cid, later_timestamp
+
+      "#{FoxySync.store_url}/checkout?fc_auth_token=#{token}&fcsid=#{fc_session_id}&fc_customer_id=#{cid}&timestamp=#{later_timestamp}"
+    end
+
+
+    private
+
+    def auth_token(customer_id, timestamp)
+      Digest::SHA1.hexdigest("#{customer_id}|#{timestamp}|#{FoxySync.api_key}")
+    end
 
   end
 end
--- spec/lib/sso_spec.rb
+++ spec/lib/sso_spec.rb
@@ -1,11 +1,36 @@
 require 'spec_helper'
 
 describe FoxySync::Sso do
+  include_examples 'api_setup'
+  include_examples 'user'
+
 
   let :sso do
     mock = double 'sso'
     mock.class.send :include, FoxySync::Sso
     mock
+  end
+
+  let :params do
+    { 'fcsid' => 'abc123', 'timestamp' => Time.now.to_i.to_s }
+  end
+
+
+  it 'should give the correct URL when no user is given' do
+    url = sso.sso_url params
+    url.should match fc_sso_url
+  end
+
+  it 'should give the correct URL when a user is given' do
+    cid = 3
+    FoxySync::Api::Customer.any_instance.stub(:customer_id).and_return cid
+    url = sso.sso_url params, user
+    url.should match fc_sso_url cid
+  end
+
+
+  def fc_sso_url(cid = 0)
+    /#{FoxySync.store_url}\/checkout\?fc_auth_token=[0-9a-z]+&fcsid=#{params['fcsid']}&fc_customer_id=#{cid}&timestamp=\d+/
   end
 
 end
