0ba1787b028567fcbd46818335639569209db5ce
Chris Stump <chris@mighty-nice.com>
2013-02-09 14:58:21 -0600

    implement datafeed handling

--- lib/foxy_sync/datafeed.rb
+++ lib/foxy_sync/datafeed.rb
@@ -1,5 +1,32 @@
+require 'rc4'
+require 'cgi'
+
 module FoxySync
+  #
+  # Holds methods that help with FoxyCart's datafeed
+  # requests (http://wiki.foxycart.com/v/1.0/webhooks).
+  # This module is typically mixed into a controller.
   module Datafeed
+
+    #
+    # Handles the decoding and decrypting of a datafeed request.
+    # Returns a +FoxySync::Api::Response+ whose document is the datafeed XML
+    #[_params_]
+    #  Something that responds_to? [] and has a key 'FoxyData'. In Rails
+    #  that would be the +params+ object
+    def datafeed_unwrap(params)
+      encrypted = params['FoxyData']
+      rc4 = RC4.new FoxySync.api_key
+      xml = rc4.decrypt CGI::unescape(encrypted)
+      FoxySync::Api::Response.new xml
+    end
+
+
+    #
+    # Wrapper for the text reply that a datafeed request expects
+    def datafeed_response
+      'foxy'
+    end
 
   end
 end
