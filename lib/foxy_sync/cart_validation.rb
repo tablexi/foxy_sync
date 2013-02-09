0364f2c3046f1dd1551ffbd1891683644b175011
Chris Stump <chris@mighty-nice.com>
2013-02-09 15:04:28 -0600

    better comments

--- lib/foxy_sync/cart_validation.rb
+++ lib/foxy_sync/cart_validation.rb
@@ -1,11 +1,19 @@
 require 'openssl'
 
 module FoxySync
+  #
+  # Holds methods to help with FoxyCart's HMAC link and form (cart) validation
+  # (http://wiki.foxycart.com/v/1.0/hmac_validation)
   module CartValidation
 
     #
-    # Hashes form input names for cart validation
-    # http://wiki.foxycart.com/v/1.0/hmac_validation
+    # Creates the value expected on name elements of form inputs
+    # [_name_]
+    #  The plain text value of the name element
+    # [_value_]
+    #  The value of the input with name +name+
+    # [_code_]
+    #  The code of the product that the input relates to
     def cart_input_name(name, value, code)
       "#{name.to_s}||#{OpenSSL::HMAC.hexdigest 'sha256', FoxySync.api_key, code.to_s + name.to_s + value.to_s}"
     end
