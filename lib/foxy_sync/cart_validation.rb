7c2d11cc3b183f4f4fd1e973bb81d0a9568b0600
Chris Stump <chris@mighty-nice.com>
2013-02-09 12:14:35 -0600

    rename #input_name

--- lib/foxy_sync/cart_validation.rb
+++ lib/foxy_sync/cart_validation.rb
@@ -6,7 +6,7 @@ module FoxySync
     #
     # Hashes form input names for cart validation
     # http://wiki.foxycart.com/v/1.0/hmac_validation
-    def input_name(name, value, code)
+    def cart_input_name(name, value, code)
       "#{name.to_s}||#{OpenSSL::HMAC.hexdigest 'sha256', FoxySync.api_key, code.to_s + name.to_s + value.to_s}"
     end
 
