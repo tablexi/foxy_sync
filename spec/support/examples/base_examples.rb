04325187a35f67d37a98b235992004d7770abf50
Chris Stump <chris@mighty-nice.com>
2013-03-03 12:03:38 -0600

    use new expect() syntax instead of .should

--- spec/support/examples/base_examples.rb
+++ spec/support/examples/base_examples.rb
@@ -1,12 +1,12 @@
 shared_examples 'base' do
   
   it 'should retain the API response' do
-    subject.api_response.should be_a FoxySync::Xml::Document
+    expect(subject.api_response).to be_a FoxySync::Xml::Document
   end
 
   it 'should delegate method_missing to the api_response' do
-    subject.api_response.should_receive(:send).with :foofam, :fiddle
-    subject.foofam(:fiddle).should be_nil
+    expect(subject.api_response).to_receive(:send).with :foofam, :fiddle
+    expect(subject.foofam(:fiddle)).to be_nil
   end
 
   it 'should delegate respond_to? to the api_response' do
