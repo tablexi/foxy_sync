d60a082e9e92d48a13b09d29966256ad92cd1708
Chris Stump <chris@mighty-nice.com>
2013-03-03 07:49:07 -0600

    pull super class NodeResponder out of Response

--- lib/foxy_sync/api/response.rb
+++ lib/foxy_sync/api/response.rb
@@ -17,48 +17,15 @@ module FoxySync::Api
   # there is one matching element in the reply, or an
   # +Array+ with all element values if there is more than one
   # element in the XML reply.
-  class Response
+  class Response < NodeResponder
     #
     # The raw XML used to create this +Response+
     attr_reader :xml
 
 
-    #
-    # A +Nokogiri::XML::Document+ of +#xml+
-    attr_reader :document
-
-
     def initialize(xml)
-      self.xml = xml
-    end
-
-
-    def xml=(xml)
       @xml = xml
-      @document = Nokogiri::XML(xml) {|config| config.strict.nonet }
-    end
-
-
-    def respond_to?(method_name, include_private = false)
-      true # respond to everything; we don't know what will be in the response doc
-    end
-
-
-    private
-
-    def method_missing(method_name, *args, &block)
-      node_set = document.xpath "//#{method_name}"
-      return nil if node_set.nil? || node_set.empty?
-
-      contents = []
-
-      node_set.children.each do |child|
-        next if child.element?
-        content = child.content.strip
-        contents << content unless content.empty?
-      end
-
-      contents.empty? ? nil : contents.size == 1 ? contents.first : contents
+      super Nokogiri::XML(xml) {|config| config.strict.nonet }
     end
   end
 end
--- spec/lib/api/response_spec.rb
+++ spec/lib/api/response_spec.rb
@@ -1,46 +1,14 @@
 require 'spec_helper'
 
 describe FoxySync::Api::Response do
-  include_examples 'api_setup'
+  include_examples 'api setup'
 
-
-  it 'should be an Api::Response' do
-    api_response.should be_a FoxySync::Api::Response
+  it 'should be a NodeResponder' do
+    expect(api_response).to be_a FoxySync::Api::NodeResponder
   end
-
-
-  it 'should make the response XML accessible' do
-    api_response.customer_email.should == 'sam@gmail.com'
-    api_response.customer_password.should match(/[a-z0-9]+/)
-    api_response.customer_id.should match(/\d+/)
-  end
-
-
-  it 'should return all matches if there is more than one' do
-    attrs = api_response.attribute
-    attrs.should be_a Array
-    attrs.should include 'mightyvite'
-    attrs.should include 'wedding'
-  end
-
-
-  it 'should be a Nokogiri document' do
-    api_response.document.should be_a Nokogiri::XML::Document
-  end
-
 
   it 'should give the raw XML' do
-    api_response.xml.should == API_REPLY
-  end
-
-
-  it 'should return nil if the response doc does not have an element matching the method called' do
-    api_response.foofam.should be_nil
-  end
-
-
-  it 'should return nil if the matched element has no text or cdata' do
-    api_response.attributes.should be_nil
+    expect(api_response.xml).to eq XmlHelper::API_REPLY
   end
 
 end
