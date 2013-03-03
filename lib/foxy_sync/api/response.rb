f617009e4cb00a2751787a27a34b721849eacc2d
Chris Stump <chris@mighty-nice.com>
2013-03-03 08:00:15 -0600

    update comments

--- lib/foxy_sync/api/response.rb
+++ lib/foxy_sync/api/response.rb
@@ -10,13 +10,6 @@ module FoxySync::Api
   # api = FoxySync::Api::Messenger.new
   # reply = api.customer_get :customer_email => 'foo@bar.com'
   # reply.customer_id # is the customer's FoxyCart id
-  #
-  # Every message will return +nil+ if no matching
-  # element is found in the XML reply or if the matched element(s)
-  # do not contain text or cdata, a +String+ if
-  # there is one matching element in the reply, or an
-  # +Array+ with all element values if there is more than one
-  # element in the XML reply.
   class Response < NodeResponder
     #
     # The raw XML used to create this +Response+
