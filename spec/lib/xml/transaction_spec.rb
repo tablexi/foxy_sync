f9f036e427d2543ef59dbfed7668145ca0f03d91
Chris Stump <chris@mighty-nice.com>
2013-03-31 12:11:02 -0500

    include base examples; keep transaction local; update tests to work with multiple details

--- spec/lib/xml/transaction_spec.rb
+++ spec/lib/xml/transaction_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe FoxySync::Xml::Transaction do
-  include_examples 'transaction'
+  include_examples 'xml document'
+
+  let :transaction do
+    FoxySync::Xml::Transaction.new XmlHelper::TRANSACTION_DECRYPTED
+  end
 
 
   it 'should return the custom product options in a hash' do
@@ -20,23 +24,22 @@ describe FoxySync::Xml::Transaction do
 
   describe 'getting all details' do
     let(:details) { transaction.details }
-    let(:detail) { details.first }
 
-    it 'should have one detail' do
-      expect(details.size).to eq 1
-      expect(details.first).to be_a FoxySync::Xml::TransactionDetail
+    it 'should have four details' do
+      expect(details.size).to eq 4
+      expect(details.all?{|detail| detail.is_a? FoxySync::Xml::TransactionDetail}).to be_true
     end
 
     it 'should have the right product_code' do
-      expect(detail.product_code).to eq transaction.product_code
+      expect(details.collect(&:product_code)).to eq transaction.product_code
     end
 
     it 'should have the right product name' do
-      expect(detail.product_name).to eq transaction.product_name
+      expect(details.collect(&:product_name)).to eq transaction.product_name
     end
 
     it 'should have the right product quantity' do
-      expect(detail.product_quantity).to eq transaction.product_quantity
+      expect(details.collect(&:product_quantity)).to eq transaction.product_quantity
     end
   end
 end
